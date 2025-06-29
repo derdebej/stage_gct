d4c46a95bbe99ff55b3e42cf3ee437ba9fb5549b
derdebej <nader.bensalah@ensi-uma.tn>
2025-06-29 12:43:10 +0100

    mise à jour de db

--- base de données/db_sql.sql
+++ base de données/db_sql.sql
@@ -17,6 +17,9 @@ CREATE TABLE Consultation (
     id_consultation INT PRIMARY KEY AUTO_INCREMENT ,
     date DATE,
     id_responsable INT,
+    nbre INT,
+    id_lot INT,
+    FOREIGN KEY (id_lot) REFERENCES lot(id_lot)
     FOREIGN KEY (id_responsable) REFERENCES utilisateur(id_utilisateur)
 );
 CREATE TABLE demande_d_achat (
@@ -32,10 +35,11 @@ CREATE TABLE demande_d_achat (
     id_utilisateur INT,
     id_consultation INT,
     chemin_da VARCHAR(255),
+    nbre INT,
     FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur),
     FOREIGN KEY (id_consultation) REFERENCES Consultation(id_consultation)
 );
-CREATE TABLE Lot (
+CREATE TABLE lot (
     id_lot INT PRIMARY KEY AUTO_INCREMENT ,
     id_da INT,
     id_article INT,
@@ -56,21 +60,31 @@ CREATE TABLE offre (
     FOREIGN KEY (id_consultation) REFERENCES Consultation(id_consultation),
     FOREIGN KEY (id_fournisseur) REFERENCES fournisseur(id_fournisseur)
 );
+CREATE TABLE offre_lot (
+    id_offre INT,
+    id_lot INT,
+    FOREIGN KEY (id_offre) REFERENCES offre,
+    FOREIGN KEY (id_lot) REFERENCES lot
+);
 CREATE TABLE evaluation (
     id_eval INT PRIMARY KEY AUTO_INCREMENT ,
     id_offre INT,
+    id_lot INT,
     chemin_evaluation TEXT,
     etat BOOLEAN DEFAULT FALSE,
-    FOREIGN KEY (id_offre) REFERENCES offre(id_offre)
+    date date,
+    conformite BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (id_offre) REFERENCES offre_lot(id_offre)
+    FOREIGN KEY (id_lot) REFERENCES offre_lot_lot(id_lot)
 );
 CREATE TABLE decision (
     id_decision INT PRIMARY KEY AUTO_INCREMENT ,
     id_lot INT UNIQUE,
     id_fournisseur INT,
-    responsable_de_la_decison INT,
-    FOREIGN KEY (id_lot) REFERENCES Lot(id_lot),
+    id_eval INT,
+    FOREIGN KEY (id_lot) REFERENCES lot(id_lot),
     FOREIGN KEY (id_fournisseur) REFERENCES fournisseur(id_fournisseur),
-    FOREIGN KEY (responsable_de_la_decison) REFERENCES utilisateur(id_utilisateur)
+    FOREIGN KEY (id_eval) REFERENCES evaluation(id_eval)
 );
 CREATE TABLE bon_commande (
     id_bc INT PRIMARY KEY AUTO_INCREMENT ,
